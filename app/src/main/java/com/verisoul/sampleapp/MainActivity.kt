cd1f50db9c9a1d3a0612ab4868dacfad39fca7c3
nketkar <niel@verisoul.xyz>
2023-11-16 10:22:52 -0600

    add api key

--- app/src/main/java/com/verisoul/sampleapp/MainActivity.kt
+++ app/src/main/java/com/verisoul/sampleapp/MainActivity.kt
@@ -19,30 +19,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val env : String = "sandbox" // or prod
-        val projectId : String = "yourVerisoulProjectId" // or prod projectId
+        val env : String = "dev" // or prod
+        val projectId : String = "00000000-0000-0000-0000-000000000001" // or prod projectId
 
         val mWebView = findViewById<WebView>(R.id.webview)
         mWebView.settings.javaScriptEnabled = true
         mWebView.addJavascriptInterface(JSBridge(this),"JSBridge")
-        mWebView.loadUrl("https://webview.$env.verisoul.xyz/?projectId=$projectId")
+//        mWebView.loadUrl("https://google.com")
+        mWebView.loadUrl("https://js.verisoul.ai/$env/webview.html/?project_id=$projectId")
     }
 
     class JSBridge(private val context: Context){
         @JavascriptInterface
-        fun verisoulMessageHandler(message: String) {
-            val apiKey : String = "yourVerisoulApiKey"
+        fun verisoulHandler(message: String) {
+            val apiKey : String = ""
             val messageJSON = JSONObject(message)
-            Toast.makeText(context, messageJSON.getString("tracking_id"), Toast.LENGTH_LONG).show()
+            Toast.makeText(context, messageJSON.getString("session_id"), Toast.LENGTH_LONG).show()
 
-            val json = JSONObject()
-            json.put("tracking_id", messageJSON.getString("tracking_id"))
-            json.put("auth_id", "yourInternalAccountIdentifier")
+            val json = JSONObject().apply {
+                put("session_id", messageJSON.getString("session_id"))
+                val accountJson = JSONObject().apply {
+                    put("id", "internal-customer-identifier")
+                }
+                put("account", accountJson)
+            }
 
             val mediaType = "application/json; charset=utf-8".toMediaType()
             val body = json.toString().toRequestBody(mediaType)
             val request = Request.Builder()
-                .url("https://api.sandbox.verisoul.xyz/zerofake/predict")
+                .url("https://api.dev.verisoul.ai/session/authenticate")
                 .addHeader("x-api-key", apiKey)
                 .post(body)
                 .build()
